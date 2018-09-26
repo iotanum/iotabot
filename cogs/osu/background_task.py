d025b524271043ad6b695bdefddea57eddb151f9
iotanum <evalduuk@gmail.com>
2018-09-26 22:36:54 +0300

    Add try/except to troubleshoot loop problems

--- cogs/osu/background_task.py
+++ cogs/osu/background_task.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from .track_management import TrackingData
 from .api_calls import api
 from .recent_scores import CheckForScores
-
+import traceback
 
 class Task:
     status = True
@@ -18,24 +18,32 @@ class Task:
         await self.bot.wait_until_ready()
         await TrackingData.update_list()
         while not self.bot.is_closed():
-            for user, channels in TrackingData.player_list.items():
-                username, user_id, pp_rank = user
-                recent_data = await CheckForScores.recent_score(int(user_id), channels)
-                if recent_data:
-                    await self.bot.get_cog("EmbedMessage").prepare_message(user, recent_data, channels)
-                    print(f"New score: {username}, Posted in: "
-                          f"{list(self.bot.get_channel(int(channel_id)).guild.name for channel_id in channels)}")
-                    await asyncio.sleep(self.per_player_sleep)
-            await asyncio.sleep(self.list_sleep)
+            try:
+                for user, channels in TrackingData.player_list.items():
+                    username, user_id, pp_rank = user
+                    recent_data = await CheckForScores.recent_score(int(user_id), channels)
+                    if recent_data:
+                        await self.bot.get_cog("EmbedMessage").prepare_message(user, recent_data, channels)
+                        print(f"New score: {username}, Posted in: "
+                              f"{list(self.bot.get_channel(int(channel_id)).guild.name for channel_id in channels)}")
+                        await asyncio.sleep(self.per_player_sleep)
+                await asyncio.sleep(self.list_sleep)
+            except Exception as e:
+                await self.bot.get_user(450567441437687818).send(f"{traceback.format_exc()}\n"
+                                                                 f"Trying to reset the loop..")
+                self.bg_task.cancel()
+                await self.restart()
+                await self.bot.get_user(450567441437687818).send(f"Restarted\n```{asyncio.Task.all_tasks()}```")
 
-    async def restart(self, ctx):
+    async def restart(self, ctx=None):
         await asyncio.sleep(2)
         self.bg_task.cancel()
         await api.reset_api_calls()
         await asyncio.sleep(3)
         self.bg_task = self.bot.loop.create_task(self.background_task())
         Task.status = True
-        print(f"Restarted the background task, requested by: {ctx.author}, from: {ctx.guild.name}")
+        if ctx:
+            print(f"Restarted the background task, requested by: {ctx.author}, from: {ctx.guild.name}")
 
     async def stop_task(self):
         self.bg_task.cancel()
