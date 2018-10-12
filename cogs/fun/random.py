574788e48c1f2263bde9d50233943f0ea95c0078
iotanum <evalduuk@gmail.com>
2018-10-12 20:41:29 +0300

    New on_message event Bot gives a notice when a user posts a youtube link with a timestamp Some readablity fixes

--- cogs/fun/random.py
+++ cogs/fun/random.py
@@ -14,21 +14,33 @@ class Fun:
     async def choose(self, ctx, *args):
         await ctx.send(random.choice(args))
 
-    async def on_message(self, message):
-        choices = ['...',
+    async def random_response_choices(self, message):
+        return ['...',
                    'LOL?',
                    "\U0001f612",
                    "\U0001f604",
                    f'{message.author.mention}',
                    'no u',
                    ';ddd']
+
+    async def on_message(self, message):
         if message.author != self.bot.user:
             Fun.counter += 1
+        choices = await self.random_response_choices(message)
         if Fun.counter == Fun.messages_until:
             await asyncio.sleep(3)
             await self.bot.get_channel(message.channel.id).send(random.choice(choices))
             Fun.counter = 0
             Fun.messages_until = random.randint(1, 5000)
+
+        await self.youtube(message)
+
+    async def youtube(self, message):
+        youtube_chars = ['youtu.be', 'youtube.com']
+        timestamp_char = 't='
+        if any(char in message.content for char in youtube_chars) and timestamp_char in message.content:
+            await asyncio.sleep(0.5)
+            await self.bot.get_channel(message.channel.id).send('\U0000261d timestamp btw')
 
 
 def setup(bot):
