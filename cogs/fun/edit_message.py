2c77f83bf8627b8ac6dedd946cff0a63d703dd9e
iotanum <evalduuk@gmail.com>
2018-11-18 22:32:36 +0200

    Add limit argument Quote command can now parse given "limit" of how many messages it should search through in the text channel history. Max limit 200 and a minimum 1. Also fixed embed coloring.

--- cogs/fun/edit_message.py
+++ cogs/fun/edit_message.py
@@ -6,8 +6,8 @@ class QuoteMessage:
     def __init__(self, bot):
         self.bot = bot
 
-    async def get_message(self, channel, word_to_edit):
-        return await self.check_for_searchable(word_to_edit, await channel.history(limit=20).flatten())
+    async def get_message(self, channel, word_to_edit, limit):
+        return await self.check_for_searchable(word_to_edit, await channel.history(limit=limit).flatten())
 
     async def check_for_searchable(self, word_to_edit, messages):
         for message in messages:
@@ -20,12 +20,19 @@ class QuoteMessage:
     async def edit_quote(self, msg_content, word_to_edit, edit_to):
         return msg_content.replace(word_to_edit, edit_to)
 
+    async def check_limit(self, num):
+        if num > 200 or num <= 0:
+            return 20
+        else:
+            return num
+
     @commands.command(name='quote', aliases=['q'])
-    async def edit_last_5_messages(self, ctx, word_to_edit, edit_to):
+    async def edit_last_5_messages(self, ctx, word_to_edit, edit_to, limit=20):
+        limit = await self.check_limit(limit)
         try:
-            message = await self.get_message(ctx.channel, word_to_edit)
+            message = await self.get_message(ctx.channel, word_to_edit, limit)
             edited_msg_content = await self.edit_quote(message.content, word_to_edit, edit_to)
-            e = discord.Embed(description=edited_msg_content, color=ctx.author.color)
+            e = discord.Embed(description=edited_msg_content, color=message.author.color)
             e.set_author(name=message.author.display_name, icon_url=message.author.avatar_url_as(format='png'))
             e.timestamp = message.created_at
             await ctx.send(embed=e)
