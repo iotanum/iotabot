d4fefc4e47b7ebad7a80e6373f722b473a83dd4b
Evaudas Macaitis <evalduuk@gmail.com>
2024-11-15 21:44:01 +0200

    chore: not byb

--- osu_tools/wrapper/dotnet_config.py
+++ osu_tools/wrapper/dotnet_config.py
@@ -1,5 +1,5 @@
 PP_CALC_DIR = "/app/osu-tools/PerformanceCalculator"
 DOTNET_VER = "net8.0"
 DOTNET_PUBLISH_RUNTIME = "linux-arm64"
-PUBLISHED_CALC = f"/app/osu-tools/publish/{DOTNET_VER}/PerformanceCalculator"
+PUBLISHED_CALC = f"bin/Debug/{DOTNET_VER}/PerformanceCalculator"
 OSU_SIMULATE_CMD = [PUBLISHED_CALC, "simulate", "osu"]
