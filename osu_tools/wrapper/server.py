9e68be8758e3d4c16325773a356c65992409efbd
Evaudas Macaitis <evalduuk@gmail.com>
2024-11-15 15:40:27 +0200

    chore: revert server.py format for pp_calc

--- osu_tools/wrapper/server.py
+++ osu_tools/wrapper/server.py
@@ -7,37 +7,25 @@ routes = web.RouteTableDef()
 
 @routes.post('/calculate')
 async def calculate(request):
-    body = json.loads(await request.text())
-    print("Calculating", body)
-
-    score = simulate_score(body['beatmap_id'], body)
-
-    scores_dict = dict()
-    scores_dict["score"] = score
-
-    # Calculate possible scores
-    body_copy = body.copy()
-    for acc in body.get("accuracy"), 100, 95, 90:
-        if acc != body_copy.get("accuracy"):
-            # Give "possible" accuracy for the calculated score
-            body_copy["accuracy"] = acc
-
-            # Remove 100s from possible score with given accuracy
-            if body_copy.get('good'):
-                del body_copy['good']
-
-            # Remove 50s from possible score with given accuracy
-            if body_copy.get('meh'):
-                del body_copy['meh']
-
-        # Give 0 misses and max combo for possible score, calculator will figure out 100s and 50s
-        body_copy['miss'] = 0
-        body_copy['combo'] = score["d_attr"]["max_combo"]
-
-        # Calculate possible score, give "acc" as key in scores_dict
-        scores_dict[acc] = simulate_score(body['beatmap_id'], body_copy)
-
-    return web.json_response(data=scores_dict)
+    needed_args = ["beatmap_id"]
+    try:
+        body = json.loads(await request.text())
+    except json.decoder.JSONDecodeError:
+        return web.json_response(data={"error": f"Empty/Invalid json."}, status=400)
+    for arg in needed_args:
+        if not body.get(arg):
+            data = {"error": f"'{arg}' is missing from the request!"}
+            return web.json_response(data=data, status=400)
+    for k, v in body.items():
+        if isinstance(v, list):
+            continue
+        body[k] = str(v).strip()
+    print(body, "calculating this")
+    print(body.get('mods'))
+    score = simulate_score(
+                           body['beatmap_id'], body
+                           )
+    return web.json_response(data=score)
 
 
 app = web.Application()
